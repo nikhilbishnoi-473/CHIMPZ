9c49330fa8e95d4c6010cddd70bcf27d89df2395
Nikhil Bishnoi <nikhilbishnoi@Nikhils-MacBook-Air.local>
2024-05-21 12:23:07 +0530

    add bottom to top

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,28 @@ function openNav() {
     document.body.classList.toggle("overflow_hidden");
     document.querySelector(".menu").classList.toggle("cross")
   }
-  
-  AOS.init(
+  // Get the button
+let mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+  AOS.init(
+{
+  duration: 1200,
+}
   );
  
